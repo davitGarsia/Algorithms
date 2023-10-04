90d449bde7d3c5d9ad68ecdd771bfcf5aa987c47
Davit Garsiashvili <davitgarsiashvili@gmail.com>
2023-10-04 12:47:08 +0400

    dp, tp

---
+++ misc/dp.js
@@ -0,0 +1,13 @@
+var maxSubArray = function (nums) {
+	const n = nums.length;
+	const dp = new Array(n);
+	dp[0] = nums[0];
+	let max = dp[0];
+
+	for (let i = 1; i < n; i++) {
+		dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+		max = Math.max(max, dp[i]);
+	}
+
+	return max;
+};
